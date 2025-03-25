ebad25ad446317e7c3e9bd0cd7aaa70dda501168
shashant <shashantyashu2004@gmail.com>
2025-03-25 11:03:19 +0530

    updated signup page

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: "https://celebrated-alfajores-a7f4c6.netlify.app", // Explicitly specify frontend origin [http://localhost:3000]  https://zerodha-clone-dashboard-kr6s.onrender.com
+    origin: "https://regal-fairy-4f653d.netlify.app/", // Explicitly specify frontend origin [http://localhost:3000]  https://zerodha-clone-dashboard-kr6s.onrender.com
     credentials: true // Allow cookies and authentication headers
   }
 ));
