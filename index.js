0d1678b03c35ba34bf42436c6bb02c8eb311d417
shashant <shashantyashu2004@gmail.com>
2025-03-25 23:29:01 +0530

    updated signup page time 2

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ app.post("/newOrder", async (req, res) => {
 
 app.get('/protected-route',  (req, res) => { //verifyToken,
   // console.log(req);
-  console.log("req.headers:", req.headers);
+  console.log("req.headers.cookie.token:", req.headers.cookie.token);
   res.json({ message: "You are authorized", user: req.user });
   // res.send(req);
 });
