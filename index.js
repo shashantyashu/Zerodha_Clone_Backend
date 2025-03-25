0142d85e1d808e28b554a712a989b03f633ad959
shashant <shashantyashu2004@gmail.com>
2025-03-25 18:43:15 +0530

    updated signup page time 2

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 // ));
 
 const allowedOrigins = [
-  'https://sunny-rolypoly-b60923.netlify.app', // your Netlify site
+  'https://singular-phoenix-dab6d6.netlify.app', // your Netlify site
   'http://localhost:3000',                  // for local dev
 ];
 
