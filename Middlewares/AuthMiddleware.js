192449e83341078c85f44870d8257f1b1f3965b1
shashant <shashantyashu2004@gmail.com>
2025-03-26 11:18:35 +0530

    updated signup page time 2

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -21,7 +21,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports.verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  // console.log(authHeader);
+  console.log(authHeader);
   const token = authHeader && authHeader.split(' ')[1];
   // console.log(token);
   if (!token) return res.status(401).json({ message: "Access denied" });
