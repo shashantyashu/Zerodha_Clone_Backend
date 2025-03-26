de4f298051a0026f193963518d776ed00bd1479e
shashant <shashantyashu2004@gmail.com>
2025-03-26 19:21:53 +0530

    updated signup page time 2

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -1,5 +1,5 @@
 // const User = require("../model/UsersModel");
-const {UsersModel} = require("../model/UsersModel");
+const { UsersModel } = require("../model/UsersModel");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -21,9 +21,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports.verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
+  // console.log(authHeader);
+  console.log("Headers:", req.headers);
+  console.log("Cookies:", req.cookies);
+  console.log("Body:", req.body);
   console.log("req.headers:", authHeader);
-  const token = authHeader.split(' ')[1]; //authHeader && 
+  const token = authHeader.split(" ")[1]; //authHeader &&
   console.log(token);
   if (!token) return res.status(401).json({ message: "Access denied" });
 
@@ -33,18 +36,17 @@ module.exports.verifyToken = (req, res, next) => {
     // next();
     jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
       if (err) {
-       return res.json(token);
+        return res.json(token);
       } else {
-        const user = await UsersModel.findById(data.id)
-        if (user) return res.json({ status: true, user: user.username })
-        else return res.json({ status: false })
+        const user = await UsersModel.findById(data.id);
+        if (user) return res.json({ status: true, user: user.username });
+        else return res.json({ status: false });
       }
-    })
+    });
   } catch (err) {
     return res.status(403).json({ message: "Invalid token" });
   }
-}
-
+};
 
 // {
 //   "email":"eva@gmail.com",
--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -2,8 +2,9 @@ const { Signup, Login } = require("../Controllers/AuthController");
 const { verifyToken } = require("../Middlewares/AuthMiddleware") //userVerification
 const router = require("express").Router();
 
+
+router.post('/',verifyToken)  //userVerification
 router.post("/signup", Signup);
 router.post('/login', Login);
-router.post('/',verifyToken)  //userVerification
 
 module.exports = router;
